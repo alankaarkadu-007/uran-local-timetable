36a515daeb00c3ed30323aa774f2c818ea16a87e
alankaarkadu-007 <alankaar.kadu@gmail.com>
2026-02-23 01:20:45 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'v26';   // 🔥 version increment करा
+const CACHE_VERSION = 'v27';   // 🔥 version increment करा
 const CACHE_NAME = `uran-local-${CACHE_VERSION}`;
 
 const FILES_TO_CACHE = [
